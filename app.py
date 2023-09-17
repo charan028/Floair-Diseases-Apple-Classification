a44dfa95ee6b5fda6871bcd5eda16d371618fdb5
charan028 <67135079+charan028@users.noreply.github.com>
2023-09-17 18:36:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,39 @@
+from flask import Flask,render_template,request
+import numpy as np
+import os
+from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from werkzeug.utils import secure_filename
-
-# define the flask app
+#define flask app
 app=Flask(__name__)
-
-# load the model
 model_path='model/apple1.h5'
 model=load_model(model_path)
-
 def model_predict(img_path,model):
     test_image=image.load_img(img_path,target_size=(224,224))
-    test_image=image.img_to_array(test_image)
-    test_image=test_image/255
+    test_image=image.img_to_array(test_img)
+    test_image=test_image/225
     test_image=np.expand_dims(test_image,axis=0)
     result=model.predict(test_image)
-    return result
-
-
+    return  result
 @app.route('/',methods=['GET'])
 def index():
-    return render_template('index.html')
-
+    return  render_template('index.html')
 @app.route('/predict',methods=['GET','POST'])
 def upload():
     if request.method=='POST':
-        # get the file from post request
+        #get the file host
         f=request.files['file']
-
-        # save the file to uploads folder
-        basepath=os.path.dirname(os.path.realpath('__file__'))
-        file_path=os.path.join(basepath,'uploads',secure_filename(f.filename))
-        f.save(file_path)
-
-        # Make prediction
-        result =model_predict(file_path,model)
-
-        categories=['Healthy','Multiple Disease','Rust','Scab']
-
-        # process your result for human
-        pred_class = result.argmax()
-        output=categories[pred_class]
+        #save the file to upload folder
+#         basepath=os.path.dirname(os.path.realpath('__file__'))
+#         file_path=os.path.join(basepath,'uploads',secure_filename(f.filename))
+#         f.save(file_path)
+        result=model_predict(f,model)
+        categories=['Healthy','Multiple disease','Rust','Scab']
+        # process for human understanding Expand Down
+    
+    
+  
+        pre_class=result.argmax()
+        output=categories[pre_class]
         return output
     return None
-
-if __name__=='__main__':
-    app.run(debug=False,port=5926)
